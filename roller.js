826aefe801892c9160188516efb6e00cd43f1144
manolck <chrisophe.noroy@live.fr>
2021-03-18 09:37:52 -0500

    +

--- roller.js
+++ roller.js
@@ -1,3 +1,4 @@
+Hooks.on("ready", function() {
   MeasuredTemplate.create({
     t: "circle",
     user: game.user._id,
@@ -7,7 +8,7 @@
     borderColor: "#FF0000",
     fillColor: "#FF3366",
   });
-
+});
 MeasuredTemplate.activateListeners(function(e){
   console.log(e);
   var center = e.center;
