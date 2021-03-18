492d62bf20c891437f3899782f912feaf31dfd82
manolck <chrisophe.noroy@live.fr>
2021-03-18 09:05:26 -0500

    test create template

--- roller.js
+++ roller.js
@@ -1,9 +1,12 @@
-console.log("Hello World! This code runs immediately when the file is loaded.");
-
-Hooks.on("init", function() {
-  console.log("This code runs once the Foundry VTT software begins it's initialization workflow.");
-});
-
-Hooks.on("ready", function() {
-  console.log("This code runs once core initialization is ready and game data is available.");
+MeasuredTemplate.create({
+  t: "cone",
+  user: game.user._id,
+  x: 1000,
+  y: 1000,
+  direction: 0.45,
+  angle: 63.13,
+  distance: 30,
+  borderColor: "#FF0000",
+  fillColor: "#FF3366",
+  texture: "tiles/fire.jpg"
 });
