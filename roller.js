46652c6de6108ad27d4069f11beebe61bf0af0c4
manolck <chrisophe.noroy@live.fr>
2021-03-18 21:13:27 -0500

    +

--- roller.js
+++ roller.js
@@ -1,18 +1,6 @@
-Hooks.on("ready", function() {
-  var template = MeasuredTemplate.create({
-    t: "circle",
-    user: game.user._id,
-    x: 2100,
-    y: 1500,
-    direction:30,
-    distance: 30,
-    borderColor: "#FF0000",
-    fillColor: "#FF3366",
+Hooks.on('canvasReady', (canvas) => {
+  canvas.templates.objects.children.forEach((template) => {
+    const t = getTemplate(template.id);
+    console.log(t);
   });
-template.then((object) => {
-  object.activateListeners((e) => {
-    console.log(e);
-  }),
-});
-
 });
