622acdc89e66dceac5286e1e568cf9f5ac1f1bc1
manolck <chrisophe.noroy@live.fr>
2021-03-18 21:56:13 -0500

    +

--- roller.js
+++ roller.js
@@ -1,6 +1,6 @@
 Hooks.on('canvasReady', (canvas) => {
   canvas.templates.objects.children.forEach((template) => {
-    const t = getTemplate(template.id);
-    console.log('id:',template.id)
+    const t = canvas.templates.get(template.id);
+    console.log('template:',t);
   });
 });
