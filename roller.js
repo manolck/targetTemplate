acdec22d3a234dbe0594f6faf746bed86c18b5fb
manolck <chrisophe.noroy@live.fr>
2021-03-20 11:39:17 -0500

    +

--- roller.js
+++ roller.js
@@ -1,4 +1,4 @@
-Hooks.on('canvasReady', (canvas) => {
+Hooks.on('MeasuredTemplateReady', (canvas) => {
   canvas.templates.objects.children.forEach((template) => {
     const t = canvas.templates.get(template.id);
     console.log('template:',t);
