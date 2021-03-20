b17c215b1b128d5a0cfb179fd284dd028ed947fc
manolck <chrisophe.noroy@live.fr>
2021-03-20 14:15:39 -0500

    +

--- roller.js
+++ roller.js
@@ -1,11 +1,12 @@
-function isCtrlPressed(){
-  document.addEventListener("keypress", function(event) {
+Hooks.on('canvasReady', (canvas) => {
+  canvas.addEventListener("keypress", function(event) {
     if (event.keyCode == 17) {
       console.log('ctrl');
     }
-  }
+  };
 }
+
 Hooks.on('hoverMeasuredTemplate', (template) => {
-    isCtrlPressed();
+
     console.log('template:',template);
 });
