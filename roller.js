9fa95cc3c0ff97d8f36767fa9ca917b9f46aeedf
manolck <chrisophe.noroy@live.fr>
2021-03-20 14:31:31 -0500

    +

--- roller.js
+++ roller.js
@@ -1,12 +1,3 @@
-Hooks.on('canvasReady', (canvas) => {
-  console.log('canvas is ready');
-  canvas.addEventListener("keypress", function(event) {
-    if (event.keyCode == 17) {
-      console.log('ctrl');
-    }
-  });
-}
-
 Hooks.on('hoverMeasuredTemplate', (template) => {
   console.log('template:',template);
 });
