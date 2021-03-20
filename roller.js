62489636d5c5021c3496b4f016d542e461eb1c88
manolck <chrisophe.noroy@live.fr>
2021-03-20 13:58:05 -0500

    +

--- roller.js
+++ roller.js
@@ -1,3 +1,11 @@
-Hooks.on('createMeasuredTemplate', (template) => {
+function isCtrlPressed(){
+  document.addEventListener("keypress", function(event) {
+    if (event.keyCode == 17) {
+      console.log('ctrl');
+    }
+  }
+}
+Hooks.on('hoverMeasuredTemplate', (template) => {
+    isCtrlPressed();
     console.log('template:',template);
 });
